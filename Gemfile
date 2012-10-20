58e24ffff389c8585a09b9ac9d761b2405d4f7df
Kim <kim@kim-desktop.(none)>
2012-10-20 13:46:39 -0400

    del warden class in gemfile

--- Gemfile
+++ Gemfile
@@ -1,13 +1,13 @@
-source 'http://isitup.org/rubygems.org'
+source :rubygems
 gem 'sinatra', '1.3.3'
 gem 'thin'
-gem 'warden'
-gem 'bcrypt-ruby'
+gem 'sqlite3'
+gem 'dm-core'
+gem 'dm-sqlite-adapter'
+gem 'dm-migrations'
+gem 'dm-validations'
+gem 'dm-timestamps'
 
 group :production do
   gem 'pg'
-end
-
-group :development, :test do
-  gem 'sqlite3'
 end
--- resume_builder.rb
+++ resume_builder.rb
@@ -1,59 +1,59 @@
 require 'sinatra'
+require 'dm-core'
+require 'dm-migrations'
+require 'dm-validations'
+require 'dm-timestamps'
 
 
 
-
-class resume_builder < Sinatra::Application
-	use Rack::Session::Cookie
-
-	# sign in page
-	get '/sign_in' do
-		return erb :sign_in
-	end
-
-	# sign in action
-	post '/sign_in' do	
-
-	end
-
-	def authenticate_user
-		# check user exists
-		# check is user's password is correct
-	end
-
-	# sign up for new accont page
-	get '/sign_up' do 
-		return erb :sign_up
-	end
-
-	post '/sign_up' do
-	end
-
-	# log out page / action
-	get '/logout' do
-
-	end
-
-
-
-	get '/' do
-	return erb :index
-	end
-
-	get '/resume' do
-		return erb :resume	
-	end
-
-	get '/webpage' do
-		return erb :webpage
-	end
-	# this will be the form
-	get '/resume_builder' do
-		return erb :resume_form
-	end
-
-	post '/create_resume' do
-		@resume = params[:resume]	
-		return erb :resume_output
-	end
+# sign in page
+get '/sign_in' do
+	return erb :sign_in
 end
+
+# sign in action
+post '/sign_in' do	
+
+end
+
+def authenticate_user
+	# check user exists
+	# check is user's password is correct
+end
+
+# sign up for new accont page
+get '/sign_up' do 
+	return erb :sign_up
+end
+
+post '/sign_up' do
+end
+
+# log out page / action
+get '/logout' do
+
+end
+
+
+
+get '/' do
+return erb :index
+end
+
+get '/resume' do
+	return erb :resume	
+end
+
+get '/webpage' do
+	return erb :webpage
+end
+# this will be the form
+get '/resume_builder' do
+	return erb :resume_form
+end
+
+post '/create_resume' do
+	@resume = params[:resume]	
+	return erb :resume_output
+end
+
